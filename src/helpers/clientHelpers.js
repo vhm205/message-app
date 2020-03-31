78310a0e122553c3b4fe7bc82d12fbbe2369f7d9
vhm205 <minhhuynhvu9221@gmail.com>
2020-03-31 14:54:16 +0700

    [32] Feature: Get newest message & handle the time send message

--- src/helpers/clientHelpers.js
+++ src/helpers/clientHelpers.js
@@ -1,3 +1,15 @@
+import moment from 'moment';
+
 export const bufferToBase64 = buffer => {
 	return Buffer.isBuffer(buffer) ? Buffer.from(buffer).toString('base64') : null;
+}
+
+export const getLastIndex = arr => {
+	if(!arr.length) return [];
+	return arr[arr.length - 1];
+}
+
+export const convertTimstampToHumanTime = timestamp => {
+	if(!timestamp) return '';
+	return moment(timestamp).locale('vi').startOf('seconds').fromNow();
 }
